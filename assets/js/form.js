57cc8bb355907e3ce3cd1205580d99cff5cd6dfe
Charles Butter <carlos@Carloss-MacBook-Air.local>
2024-04-21 21:12:53 -0600

    added input form event listener and new variables to capture data

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,13 @@
-const usernameInput = document.querySelector('#username');
+const userNameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#title');
 const messageInput = document.querySelector('#messageBox');
+const inputForm = document.querySelector('#inputForm');
 
+inputForm.addEventListener('submit', function(event){
+    event.preventDefault(); 
+   const userName = userNameInput.value;
+   const title = titleInput.value;
+   const message = messageInput.value;
+   
+})
 
