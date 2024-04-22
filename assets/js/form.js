55a25f1b1d53b990e1b36c96fa74525724a25d66
Charles Butter <carlos@Carloss-MacBook-Air.local>
2024-04-21 23:17:33 -0600

    fixed the link to the script .js file

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,2 +1,6 @@
 const form = document.getElementById('inputForm');
-form.addEventListener('submit', function(event));
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+    const usernameInput = document.getElementById('username').value;
+    console.log(usernameInput); 
+})
