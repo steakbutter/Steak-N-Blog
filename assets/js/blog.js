1d163684752eafa9f6fc46afdd02a0454d857078
Charles Butter <carlos@Carloss-MacBook-Air.local>
2024-04-25 12:21:03 -0600

    Added if loop to toggle light and dark mode

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,4 +3,14 @@ const container = document.querySelector('.container');
 
 let mode = 'light';
 
+sunEmoji.addEventListener ('click', function() {
+    if (mode === 'light') {
+        mode = "dark";
+        container.setAttribute('class', 'dark');  
+    }
+    else {
+        mode = 'light';
+        container.setAttribute('class', 'light');
+    }
+});
 
